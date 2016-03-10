15ed797ccca65554d78e6ab15b1a77d499eefe66
Chris Jenkins <chrisalanjenkins@gmail.com>
2016-03-10 15:58:07 +0000

    completed ex 1

---
+++ Ex. 1 Hello World/HelloWorld.java
@@ -0,0 +1,13 @@
+/*
+ * Hello World - without variables, there might 
+ * be a better solution and this might perhaps 
+ * not count as not having variables
+ */
+
+public class HelloWorld {		
+		public static void main(String[] args){
+			System.out.println("Hello " + args[0]);
+		}
+	}
+
+

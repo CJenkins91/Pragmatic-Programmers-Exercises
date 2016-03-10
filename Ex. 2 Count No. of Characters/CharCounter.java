754bd2411bcf1df89ac1d74705a84f7f858b9c10
Chris Jenkins <chrisalanjenkins@gmail.com>
2016-03-10 16:11:49 +0000

    completed ex 2

---
+++ Ex. 2 Count No. of Characters/CharCounter.java
@@ -0,0 +1,15 @@
+/*
+ * Char Counter - takes in a string and returns the
+ * string and the number of characters in the string 
+ */
+
+public class CharCounter {		
+		
+	public static void main(String[] args){
+			
+		System.out.println("The supplied string is " + args[0] 
+					+ ", it has " + args[0].length() + " characters");
+	}
+}
+
+
